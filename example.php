2da72c6d560658dc0bfd8d75b66fa1f8ea8fbc78
Eduardo Leoni <eduardo@pixelfusion.co.nz>
2016-12-15 09:47:07 +1300

    Apply changes to complain with PSR-4

--- example.php
+++
@@ -1,5 +0,0 @@
-<?php
-include ("DotenvSet.class.php");
-
-$env = new DotenvSet;
-$env->set("DB_PASS", "root");
--- src/dotenv-set/DotenvSet.php
+++ src/dotenv-set/DotenvSet.php
@@ -1,42 +1,61 @@
 <?php
+
+namespace DotenvSet;
+
 class DotenvSet
 {
-	public $path;
+	public $path = "../../../.env";
 
-	public function __construct($path="./.env") {
-		$this->path = $path;
-	}
-
-	public function getPath() {
+	public static function getPath()
+	{
 		return $this->path;
 	}
 
-	public function set($variable, $value) {
+	public static function setPath($path)
+	{
+		$this->path = $path;
+	}
+
+	public function set($variable, $value, $path=null)
+	{
+		if ($path) {
+			$this->path = $path;
+		}
+
 		$string = "$variable=$value";
 		$get = $this->getDotEnv();
-		if (strpos($get, $variable) != true){
+
+		if (strpos($get, $variable) != true) {
 			file_put_contents($this->path, file_get_contents($this->path) . "\n" . $string);
 		} else {
+
 			$all = explode("\n", $get);
 			$env = "";
+
 			foreach ($all as $each) {
 				$current = explode("=", $each);
 				if ($current[0] == $variable) {
 					$each = $current[0] . "=$value";
 				}
+
 				$env .= $each . "\n";
 			}
+
 			file_put_contents($this->path, $env);
+
 		}
 	}
 
-	public function getDotEnv() {
+	public static function getDotEnv()
+	{
 		$this->createIfDoesntExist();
 		$env = file_get_contents($this->path);
+
 		return $env;
 	}
 
-	public function createIfDoesntExist() {
+	public static function createIfDoesntExist()
+	{
 		if (!file_exists($this->path)) {
 			file_put_contents($this->path, "");
 		}
